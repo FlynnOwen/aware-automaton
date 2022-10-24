30a5356e2cf117b0ef9403784e2a7e38899f8f18
Flynn Owen <flynnoo49@gmail.com>
2022-10-24 22:22:49 +1300

    polish card

--- flynn-website/src/components/cards/profile.js
+++ flynn-website/src/components/cards/profile.js
@@ -3,22 +3,36 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, Grid } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
 
 export default function ProfileCard() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 345}} >
       <CardActionArea>
         <CardMedia
           component="img"
-          height="140"
+          height="300"
           image={require("../../images/Blinkie.jpg")}
           alt="Binkie"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div" textAlign="center">
+          <Typography gutterBottom variant="h5" component="div" textAlign="center" alignItems="center">
             Flynn Owen
           </Typography>
+          <Grid container item justifyContent="center">
+            <Grid item xs={2}>
+            <GitHubIcon/>
+            </Grid>
+            <Grid item xs={2}>
+            <LinkedInIcon/>
+            </Grid>
+            <Grid item xs={2}>
+            <EmailIcon/>
+            </Grid>
+          </Grid>
           <Typography variant="body2" color="text.secondary">
             What a dude I am. Look at me, I am one hell of a dude.
           </Typography>
