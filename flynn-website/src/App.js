b52ab07c24b7558a6a3ca47ecbeb404d7230a547
Flynn Owen <flynnoo49@gmail.com>
2022-10-24 20:34:03 +1300

    add docs for structure

--- flynn-website/src/App.js
+++ flynn-website/src/App.js
@@ -1,9 +1,43 @@
 import './App.css';
-import { Button } from '@material-ui/core';
+import { AppBar, IconButton, MenuItem, Toolbar, Typography } from '@material-ui/core';
 
 
 function App() {
-  return <Button color="primary">Hello World</Button>;
+  // <Navbar/>
+  // <div>
+  // <profile img/>
+  // <Contact card/>
+  // </div>
+  // <div>
+  // <experience-list>
+  // <experience/>
+  // <experience/>
+  // </experience-list>
+  // </div>
+  // <div>
+  // <education-list>
+  // <education/>
+  // <education/>
+  // </education-list>
+  // </div>
+  // <div>
+  // <project-grid>
+  // <project-card>
+  // <project-card>
+  // <project-card>
+  // </project-grid>
+  // </div>
+
+  return (<AppBar position="static">
+    <Toolbar variant="dense">
+      <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+        <MenuItem />
+      </IconButton>
+      <Typography variant="h6" color="inherit" component="div">
+        Photos
+      </Typography>
+    </Toolbar>
+  </AppBar>);
 }
 
 export default App;
