b9920eb9bb8d7c4d447de45cd0b25d6477b9062a
Flynn Owen <flynnoo49@gmail.com>
2022-10-25 15:02:38 +1300

    add body

---
+++ flynn-website/src/components/body/body.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Paper } from '@mui/material';
+import { makeStyles } from '@material-ui/core/styles';
+
+function CustomBody() {
+    
+  return (
+    <Paper elevation={3} sx={{ mt: 5, width: '50%'}}>
+        Test
+    </Paper>
+  );
+}
+
+export default CustomBody;
