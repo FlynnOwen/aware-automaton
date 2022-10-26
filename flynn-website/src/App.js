50f399466397de0db107e7214d551e797b316185
Flynn Owen <flynnoo49@gmail.com>
2022-10-26 14:26:31 +1300

    nicer spacin

--- flynn-website/src/App.js
+++ flynn-website/src/App.js
@@ -3,46 +3,56 @@ import ProfileCard from './components/cards/profile';
 import CustomAppBar from './components/navbar/appbar';
 import CustomBody from './components/body/body';
 import { Box } from '@mui/material';
-import { Route } from 'react-router';
+import { Redirect, Route } from 'react-router';
+import NotFound from './pages/notfound';
 
 
 function App() {
 
   return (
     <div>
+
+      <Route path="/">
+      <CustomAppBar/>
+      </Route>
+
+      <Route path="/" exact>
+        <Redirect to='/home' />
+      </Route>
+
+      <Route path="/home">
+      <div>
+      <Box sx={{display: 'flex'}}>
+        <ProfileCard/>
+      </Box>
+      </div>
+    </Route>
+
       <Route path="/about">
         <div>
-        <CustomAppBar/>
         <Box sx={{display: 'flex'}}>
           <ProfileCard/>
           <CustomBody/>
         </Box>
         </div>
       </Route>
-      <Route path="/home">
-      <div>
-      <CustomAppBar/>
-      <Box sx={{display: 'flex'}}>
-        <ProfileCard/>
-      </Box>
-      </div>
-    </Route>
+
     <Route path="/blog">
       <div>
-      <CustomAppBar/>
       <Box sx={{display: 'flex'}}>
         <ProfileCard/>
       </Box>
       </div>
     </Route>
+
     <Route path="/projects">
       <div>
-      <CustomAppBar/>
       <Box sx={{display: 'flex'}}>
         <ProfileCard/>
       </Box>
       </div>
     </Route>
+
   </div>
   );
 }
