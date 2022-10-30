5f9d3239f53af3b0e04eccef0cca44ee651f145f
Flynn Owen <flynnoo49@gmail.com>
2022-10-30 23:39:58 +1300

    change padding on accordians

--- flynn-website/src/components/body/academicaccordian.js
+++ flynn-website/src/components/body/academicaccordian.js
@@ -23,10 +23,10 @@ export default function AcademicAccordion(props) {
           <Button variant="outlined" href="https://openaccess.wgtn.ac.nz/articles/thesis/Finite_Mixture_Models_-_A_Divide_and_Conquer_Approach/21300270/1" startIcon={<BookIcon/>} sx={{mb: 3}}>
             Thesis
             </Button>
-          <Typography sx={{color: 'text.secondary', mb: 2}}>
+          <Typography sx={{color: 'text.secondary'}}>
               Grade: Distinction
             </Typography>
-            <Typography>
+            <Typography sx={{ mt: 2, mb: 2 }}>
             Finite Mixture Models - A Divide and Conquer Approach. Funded by a Marsden scholarship, and Supervised by 
             Prof. Richard Arnold and Dr. Louise McMillan, my thesis investigated a novel, computationally efficient
             approach to applying model-based clustering to datasets with unknown numbers of mixture components.
@@ -43,10 +43,10 @@ export default function AcademicAccordion(props) {
             <Typography sx={{ width: '66%'}}>Masters of Applied Statistics</Typography>
           </AccordionSummary>
           <AccordionDetails>
-          <Typography sx={{color: 'text.secondary', mb: 2}}>
+          <Typography sx={{color: 'text.secondary'}}>
               Grade: Distinction
             </Typography>
-            <Typography>
+            <Typography sx={{ mt: 2, mb: 2 }}>
               A course-based post graduate degree. My courses focussed heavily on computational statistics, machine learning and data science.
               An outstanding result in the research component of this degree awarded me a scholarship to complete the Masters of Science degree.
             </Typography>
@@ -62,7 +62,7 @@ export default function AcademicAccordion(props) {
             <Typography sx={{ width: '66%'}}>Bachelors (Mathematics, Economics)</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
+            <Typography sx={{ mt: 2, mb: 2 }}>
               My majors were mathematics and economics for my undergraduate, bachelors degree. Math courses were heavily focussed in both
               discrete and pure mathematics, while my economics courses were a combination of both macro and micro, but having heavy math influence.
             </Typography>
--- flynn-website/src/components/body/workaccordian.js
+++ flynn-website/src/components/body/workaccordian.js
@@ -19,7 +19,7 @@ export default function WorkAccordion(props) {
           <Typography sx={{ width: '33%', color: 'text.secondary' }}>04/21 - Present</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
+          <Typography sx={{ mt: 2, mb: 2 }}>
             Ingenuitive and tight knit team doing cool things with data. Some of my work has included working the DDI - an index
             measuring deprivation around NZ. The DCT - an index measuring carbon emissions at a daily frequency around NZ. And
             a large amount of ETL jobs using Jenkins, Python, Selenium and PostgreSQL.
@@ -37,7 +37,7 @@ export default function WorkAccordion(props) {
           <Typography sx={{ width: '33%', color: 'text.secondary' }}>01/20 - 04/21</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
+          <Typography sx={{ mt: 2, mb: 2 }}>
             Built the foundations of Spotlight Reportings data infrastructure in AWS, including a data lake and data warehouse.
             Deployed a productionised churn and conversion model for the sales and customer success teams to use, with dashboards 
             visualising relevant data. Created a proof concept time series prediction product analogous to a 'three way forecast'
@@ -56,7 +56,7 @@ export default function WorkAccordion(props) {
           <Typography sx={{ width: '33%', color: 'text.secondary' }}>11/19 - 12/19</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
+          <Typography sx={{ mt: 2, mb: 2 }}>
             A small internship were I worked on creating a dashboard for a large NZ sports streaming service. 
             All written in R Shiny.
           </Typography>
@@ -73,7 +73,7 @@ export default function WorkAccordion(props) {
           <Typography sx={{ width: '33%', color: 'text.secondary' }}>11/18 - 02/19</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
+          <Typography sx={{ mt: 2, mb: 2 }}>
             An internship within the IT department of Sealord, where I did reporting in Excel and SQL to provide insights to the sales team.
           </Typography>
         </AccordionDetails>
