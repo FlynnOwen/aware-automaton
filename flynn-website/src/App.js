ea3419b6a5317ad6bf1f7b1657ad0f201d4f5040
Flynn Owen <flynnoo49@gmail.com>
2022-10-30 17:43:03 +1300

    restructure app.js

--- flynn-website/src/App.js
+++ flynn-website/src/App.js
@@ -12,62 +12,61 @@ function App() {
 
   return (
     <div>
-      <CustomParticles/>
-      <Route path="/">
-        <CustomAppBar/>
-      </Route>
+      <CustomParticles />
+      <CustomAppBar />
 
       <Switch>
-      <Route path="/" exact>
-        <Redirect to='/home' />
-      </Route>
+        <Route path="/" exact>
+          <Redirect to='/home' />
+        </Route>
 
-      <Route path="/home">
-      <div style={{
-           position: 'absolute', left: '50%', top: '50%',
-           transform: 'translate(-50%, -50%)'}}>
-        <Fade in={true} timeout={{enter: 2500}}>
-        <Typography variant='h2' color="white">&#123;Aware Automaton&#125;</Typography>
-        </Fade>
-      </div>
-      <div style={{
-           position: 'absolute', left: '50%', top: '60%',
-           transform: 'translate(-50%, -50%)'}}>
-        <Fade in={true} timeout={{enter: 5000}}>
-        <Typography variant='h6' color="grey">A scratchpad for Flynn Owen</Typography>
-        </Fade>
-      </div>
-    </Route>
+        <Route path="/home">
+          <div style={{display: "flex", flexDirection: "column", textAlign: "center", marginTop: "35vh"}}>
 
-      <Route path="/about">
-        <div>
-        <Box sx={{display: 'flex'}}>
-          <ProfileCard/>
-          <CustomBody/>
-        </Box>
-        </div>
-      </Route>
+            <div style={{
+            }}>
+              <Fade in={true} timeout={{ enter: 2500 }}>
+                <Typography variant='h2' color="white">&#123;Aware Automaton&#125;</Typography>
+              </Fade>
+            </div>
+            <div style={{
+            }}>
+              <Fade in={true} timeout={{ enter: 5000 }}>
+                <Typography variant='h6' color="grey">A scratchpad for Flynn Owen</Typography>
+              </Fade>
+            </div>
+          </div>
+        </Route>
 
-    <Route path="/blog">
-      <div>
-      <Box sx={{display: 'flex'}}>
-        <ProfileCard/>
-        <CustomBlogBody/>
-      </Box>
-      </div>
-    </Route>
+        <Route path="/about">
+          <div>
+            <Box sx={{ display: 'flex' }}>
+              <ProfileCard />
+              <CustomBody />
+            </Box>
+          </div>
+        </Route>
 
-    <Route path="/projects">
-      <div>
-      <Box sx={{display: 'flex'}}>
-        <ProfileCard/>
-        <CustomProjectsBody/>
-      </Box>
-      </div>
-    </Route>
-    </Switch>
+        <Route path="/blog">
+          <div>
+            <Box sx={{ display: 'flex' }}>
+              <ProfileCard />
+              <CustomBlogBody />
+            </Box>
+          </div>
+        </Route>
 
-  </div>
+        <Route path="/projects">
+          <div>
+            <Box sx={{ display: 'flex' }}>
+              <ProfileCard />
+              <CustomProjectsBody />
+            </Box>
+          </div>
+        </Route>
+      </Switch>
+
+    </div>
   );
 }
 
