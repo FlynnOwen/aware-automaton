fc84b31233883483d100f453c3553b3ee4ade264
Flynn Owen <flynnoo49@gmail.com>
2022-11-02 20:58:42 +1300

    add blog nav

---
+++ flynn-website/src/pages/blog/blognav.js
@@ -0,0 +1,30 @@
+import { Divider, Fade, List, ListItem, ListItemText, Paper, Typography } from '@mui/material';
+import * as React from 'react';
+import BlogPost from './blogpost';
+
+const style = {
+    maxWidth: 360,
+    bgcolor: 'background.paper',
+  };
+
+function BlogNav(props) {
+    
+  return (
+    <Fade in={true} timeout={{enter: 1000}}>
+    <Paper sx={{m: 5, height:500, width: 250, textAlign:"center"}}>
+        <Typography variant="h4" sx={{ m:3 }}> Blog Posts </Typography>
+    <List sx={style} component="nav" aria-label="mailbox folders">
+        
+    {props.blogPosts.map((step, index) => (
+        <ListItem button onClick={() => { props.handleIndex(index); }}>
+        <ListItemText primary={step.title} sx={{textAlign: "center"}}/>
+      </ListItem>
+    ))}
+
+    </List>
+    </Paper>
+    </Fade>
+  );
+}
+
+export default BlogNav;
