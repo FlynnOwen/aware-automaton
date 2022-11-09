1e45f649bd0efa62e408480fd6a5b5b484b89829
Flynn Owen <flynnoo49@gmail.com>
2022-11-09 16:46:17 +1300

    finish thesis blog

--- flynn-website/src/pages/blog/blogpage.js
+++ flynn-website/src/pages/blog/blogpage.js
@@ -82,35 +82,88 @@ const blogPosts = [
     a minimum, or the LRTS is rejected.
     <br/>
     <br/>
+    To test this algorithm, we used simulated data. To generate this data, we can specify a number of parameters:
+    <br/>
+    <ul>
+        <li><Latex>{`$$R$$`}</Latex> : The number of clusters or mixtures in the dataset. </li>
+        <li><Latex>{`$$n$$`}</Latex> : The number of rows of the matrix Y. </li>
+        <li><Latex>{`$$m$$`}</Latex> : The number of columns of the matrix Y. </li>
+        <li><Latex>{`$$\\theta$$`}</Latex> : A vector of length R, which are the GLM parameters for each mixture. </li>
+        <li><Latex>{`$$\\pi$$`}</Latex> : A vector of length R, which are the mixing proportions. </li>
+    </ul>
+    <br/>
+    By using these parameters to randomly generate data matrices, we can fit them with a series of algorithms to see how well they estimate the parameters <Latex>{`$$\\theta$$`}</Latex> and <Latex>{`$$\\pi$$`}</Latex>.
+    For the following examples, we simulated a dataset with the following parameters:
+    <br/>
+    <ul>
+        <li><Latex>{`$$R$$`}</Latex> = 3 </li>
+        <li><Latex>{`$$n$$`}</Latex> = 800 </li>
+        <li><Latex>{`$$m$$`}</Latex> = 200 </li>
+        <li><Latex>{`$$\\theta$$`}</Latex> = (0.25, 0.5, 0.75) </li>
+        <li><Latex>{`$$\\pi$$`}</Latex> = (0.25, 0.5, 0.25) </li>
+    </ul>
+    <br/>
+    Our first algorithm is named 'DMLL', which is inspired by the method of fitting a model with R = 1 mixture, calculating the AIC, and incrementing R = R + 1 until the AIC reaches a minimum.
+    Figure 3 shows the allocation of datapoints to clusters when fitting the DMLL with R = 1 model on the left, followed by R = 2, and finally R = 3, which it halted on, showing the DMLL's optimal number of clusters and allocation.
     <Box sx={{m: 3}}>
     <Box component="img" src={require('../../images/blog/DMLL_sankey_800.png')} height={300}></Box>
     <Typography>Figure 3: A Sankey diagram of DMLL.</Typography>
     </Box>
-
+    To develop a new algorithm, we take inspiration from 'divide and conquer' methods from computer science, as well as 'hierarchical clustering' used in common, distance based methods.
+    The idea behind the algorithm is that mixtures are necessarily composed of sub-mixtures. We first fit the full dataset with both R = 1 cluster and R = 2 clusters. If the AIC is lower for the R = 1 fit than the R = 2 fit, we halt 
+    - this is the optimal number of clusters. If not, We then perform an E-step to find allocation of datapoints to mixtures. The full dataset is then subsetted based on this membership.
+    For each of the sub-datasets, we perform both R = 1 and R = 2 fits, and if the AIC for the R = 1 fit is lower than the R = 2 fit, then this branch has halted. This algorithm is named the 'HNTB' algorithm.
+    <br/>
+    <br/>
+    Figure 4 shows the Sankey diagram for this algorithm when applied on the same dataset as was used in Figure 3. Notice that mixtures are necessarily composed of sub-mixtures as the Sankey diagram proceeds.
     <Box sx={{m: 3}}>
     <Box component="img" src={require('../../images/blog/HNTB_sankey_800.png')} height={300}></Box>
     <Typography>Figure 4: A Sankey diagram of HNTB.</Typography>
     </Box>
-
+    Clearly, compared to the DMLL method, the HNTB method has over-estimated R by 1, and the <Latex>{`$$\\pi$$`}</Latex> values are quite different to the true values used to simulate the dataset.
+    To counter address this limitation, once the HNTB method has halted, we execute a single E-step, borrowed from the EM algorithm, and then re-fit each seperate dataset, dependent on this membership with a single R = 1 model.
+    This algorithm is named the 'HSR' algorithm - the result of this is shown in figure 5:
     <Box sx={{m: 3}}>
     <Box component="img" src={require('../../images/blog/HSR_sankey_800.png')} height={300}></Box>
     <Typography>Figure 5: A Sankey diagram of HSR.</Typography>
     </Box>
-    
+    This addition of the E-step at the end of HNTB has reallocated several datapoints from the fourth, smallest cluster into the second smallest cluster. 
+    Clearly, this smallest cluster in the HNTB may have been an extra, 'dud' cluster, whose datapoints are better suited in another cluster. The results are looking 
+    more similar to both the DMLL and true parameters than the HNTB were. Perhaps, more reallocation may benefit the algorithm even more? We define a new algorithm, the
+    HRL algorithm, that reallocates datapoints continually until no more can be reallocated (convergence of the E-step). Results are shown in Figure 6:
     <Box sx={{m: 3}}>
     <Box component="img" src={require('../../images/blog/HRL_sankey_800.png')} height={300}></Box>
     <Typography>Figure 6: A Sankey diagram of HRL.</Typography>
     </Box>
-    
+    The HRL algorithm has converged with R = 3, the same as both the DMLL and the true parameter! Not only this, but the <Latex>{`$$\\pi$$`}</Latex> values are the exact same as the DMLL also!
+    If this method is able to reliably execute faster than the DMLL algorithm, we could conclude that a divide and conquer approach to mixture modelling is both faster and suitbaly replicates traditional methods.
+    We define a final method, for interest, that reallocates datapoints while the hierarchical process is still executing (not strictly reallocating at the end). This method is named the HRD algorithm, results are shown in
+    Figure 7:
+    <Box sx={{m: 3}}>
+    <Box component="img" src={require('../../images/blog/HEM_sankey_800.png')} height={300}></Box>
+    <Typography>Figure 7: A Sankey diagram of HRD.</Typography>
+    </Box>
+    Unfortunately, this method hasn't addressed the problem of there being too many estimated clusters, and there remains R = 4, the same the HNTB algorithm.
+    <br/>
+    <br/>
+    To accurately assess these methods, a large-scale simulation study was run. To test how each algorithm scaled with the number of clusters, R, we generated data,
+    fit the data using each algorithm, and reported on how well they estimated both the true number of clusters, and how long they took to run. The full protocol used in the
+    simulation is shown here:
+    <Box sx={{m: 3}}>
+    <Box component="img" src={require('../../images/blog/Simulation_Protocol.png')} height={180}></Box>
+    </Box>
+    Figure 8 shows that the DMLL algorithm seems to constantly under-estimate R, compared to the truth - especially as R increases. HNTB and HSR look to generally over-estimate R. Closest to the truth are both HRL and HRD.
     <Box sx={{m: 3}}>
     <Box component="img" src={require('../../images/blog/R_Values_line_Study3.png')} height={300}></Box>
-    <Typography>Figure 7: How well the different algorithms approximated true R.</Typography>
+    <Typography>Figure 8: How well the different algorithms approximated true R.</Typography>
     </Box>
-
+    Figure 9 shows that the DMLL algorithm scales the worst the the number of clusters by far. Less reallocation means less run time - as evidenced by the HNTB algorithm being the fastest.
     <Box sx={{m: 3}}>
     <Box component="img" src={require('../../images/blog/Run_times_line_Study3.png')} height={300}></Box>
-    <Typography>Figure 8: Comparison of run time in seconds of the different algorithms.</Typography>
-    </Box>
+    <Typography>Figure 9: Comparison of run time in seconds of the different algorithms.</Typography>
+    </Box> 
+    Findings from the research were that these novel rapid mixture modelling techniques were much faster than traditional methods. As well as this, they fitted the true data well, in cases even better than
+    the traditional methods, as shown in Figure 8 as DMLL consistently under-predicted R. Make sure to view the code under the 'projects' page, or to read my thesis on the 'about' page under the thesis dropdown.
 
     </Typography>
 },
