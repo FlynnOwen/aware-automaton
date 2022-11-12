a2768a4c9c25fdbc362f1d18d9d0eaa50feb7875
Flynn Owen <flynnoo49@gmail.com>
2022-11-13 00:13:11 +1300

    add % width to blogpost

--- flynn-website/src/pages/blog/blogpage.js
+++ flynn-website/src/pages/blog/blogpage.js
@@ -17,14 +17,14 @@ export const blogPosts = [
     and independent variables which describe these individuals. Clustering assigns these individuals into groups, such that individuals
     in the same group share similar independent variables, and individuals in different groups are different in terms of these independent variables.
     <Box sx={{m: 3}}>
-    <Box component="img" src={require('../../images/blog/Base_R=1.png')} height={300}/>
+    <Box component="img" src={require('../../images/blog/Base_R=1.png')} width={'90%'}/>
     <Typography>Figure 1: A Gaussian density.</Typography>
     </Box>
     Figure 1, above, shows a Gaussian (normal) density of a variable y. Looking at the clear peaks in the density plot, it appears that this
     density is 'tri-modal', meaning that there appear to be 3 'modes'. If we were to group this dataset into a number of different datapoints, 
     3 appears to be a reasonable number, and a method we might use is called 'finite mixture modelling'.
     <Box sx={{m: 3}}>
-    <Box component="img" src={require('../../images/blog/Split_Full.png')} height={300}></Box>
+    <Box component="img" src={require('../../images/blog/Split_Full.png')} width={'90%'}></Box>
     <Typography>Figure 2: A Gaussian density with 3 components.</Typography>
     </Box>
     Finite mixture modelling in the general sense requires you to choose a set of arguments before fitting the model.
@@ -103,7 +103,7 @@ export const blogPosts = [
     Our first algorithm is named 'DMLL', which is inspired by the method of fitting a model with R = 1 mixture, calculating the AIC, and incrementing R = R + 1 until the AIC reaches a minimum.
     Figure 3 shows the allocation of datapoints to clusters when fitting the DMLL with R = 1 model on the left, followed by R = 2, and finally R = 3, which it halted on, showing the DMLL's optimal number of clusters and allocation.
     <Box sx={{m: 3}}>
-    <Box component="img" src={require('../../images/blog/DMLL_sankey_800.png')} height={300}></Box>
+    <Box component="img" src={require('../../images/blog/DMLL_sankey_800.png')} width={'90%'}></Box>
     <Typography>Figure 3: A Sankey diagram of DMLL.</Typography>
     </Box>
     To develop a new algorithm, we take inspiration from 'divide and conquer' methods from computer science, as well as 'hierarchical clustering' used in common, distance based methods.
@@ -114,14 +114,14 @@ export const blogPosts = [
     <br/>
     Figure 4 shows the Sankey diagram for this algorithm when applied on the same dataset as was used in Figure 3. Notice that mixtures are necessarily composed of sub-mixtures as the Sankey diagram proceeds.
     <Box sx={{m: 3}}>
-    <Box component="img" src={require('../../images/blog/HNTB_sankey_800.png')} height={300}></Box>
+    <Box component="img" src={require('../../images/blog/HNTB_sankey_800.png')} width={'90%'}></Box>
     <Typography>Figure 4: A Sankey diagram of HNTB.</Typography>
     </Box>
     Clearly, compared to the DMLL method, the HNTB method has over-estimated R by 1, and the <Latex>{`$$\\pi$$`}</Latex> values are quite different to the true values used to simulate the dataset.
     To counter address this limitation, once the HNTB method has halted, we execute a single E-step, borrowed from the EM algorithm, and then re-fit each seperate dataset, dependent on this membership with a single R = 1 model.
     This algorithm is named the 'HSR' algorithm - the result of this is shown in figure 5:
     <Box sx={{m: 3}}>
-    <Box component="img" src={require('../../images/blog/HSR_sankey_800.png')} height={300}></Box>
+    <Box component="img" src={require('../../images/blog/HSR_sankey_800.png')} width={'90%'}></Box>
     <Typography>Figure 5: A Sankey diagram of HSR.</Typography>
     </Box>
     This addition of the E-step at the end of HNTB has reallocated several datapoints from the fourth, smallest cluster into the second smallest cluster. 
@@ -129,7 +129,7 @@ export const blogPosts = [
     more similar to both the DMLL and true parameters than the HNTB were. Perhaps, more reallocation may benefit the algorithm even more? We define a new algorithm, the
     HRL algorithm, that reallocates datapoints continually until no more can be reallocated (convergence of the E-step). Results are shown in Figure 6:
     <Box sx={{m: 3}}>
-    <Box component="img" src={require('../../images/blog/HRL_sankey_800.png')} height={300}></Box>
+    <Box component="img" src={require('../../images/blog/HRL_sankey_800.png')} width={'90%'}></Box>
     <Typography>Figure 6: A Sankey diagram of HRL.</Typography>
     </Box>
     The HRL algorithm has converged with R = 3, the same as both the DMLL and the true parameter! Not only this, but the <Latex>{`$$\\pi$$`}</Latex> values are the exact same as the DMLL also!
@@ -137,7 +137,7 @@ export const blogPosts = [
     We define a final method, for interest, that reallocates datapoints while the hierarchical process is still executing (not strictly reallocating at the end). This method is named the HRD algorithm, results are shown in
     Figure 7:
     <Box sx={{m: 3}}>
-    <Box component="img" src={require('../../images/blog/HEM_sankey_800.png')} height={300}></Box>
+    <Box component="img" src={require('../../images/blog/HEM_sankey_800.png')} width={'90%'}></Box>
     <Typography>Figure 7: A Sankey diagram of HRD.</Typography>
     </Box>
     Unfortunately, this method hasn't addressed the problem of there being too many estimated clusters, and there remains R = 4, the same the HNTB algorithm.
@@ -147,16 +147,16 @@ export const blogPosts = [
     fit the data using each algorithm, and reported on how well they estimated both the true number of clusters, and how long they took to run. The full protocol used in the
     simulation is shown here:
     <Box sx={{m: 3}}>
-    <Box component="img" src={require('../../images/blog/Simulation_Protocol.png')} height={180}></Box>
+    <Box component="img" src={require('../../images/blog/Simulation_Protocol.png')} width={'90%'}></Box>
     </Box>
     Figure 8 shows that the DMLL algorithm seems to constantly under-estimate R, compared to the truth - especially as R increases. HNTB and HSR look to generally over-estimate R. Closest to the truth are both HRL and HRD.
     <Box sx={{m: 3}}>
-    <Box component="img" src={require('../../images/blog/R_Values_line_Study3.png')} height={300}></Box>
+    <Box component="img" src={require('../../images/blog/R_Values_line_Study3.png')} width={'90%'}></Box>
     <Typography>Figure 8: How well the different algorithms approximated true R.</Typography>
     </Box>
     Figure 9 shows that the DMLL algorithm scales the worst the the number of clusters by far. Less reallocation means less run time - as evidenced by the HNTB algorithm being the fastest.
     <Box sx={{m: 3}}>
-    <Box component="img" src={require('../../images/blog/Run_times_line_Study3.png')} height={300}></Box>
+    <Box component="img" src={require('../../images/blog/Run_times_line_Study3.png')} width={'90%'}></Box>
     <Typography>Figure 9: Comparison of run time in seconds of the different algorithms.</Typography>
     </Box> 
     Findings from the research were that these novel rapid mixture modelling techniques were much faster than traditional methods. As well as this, they fitted the true data well, in cases even better than
