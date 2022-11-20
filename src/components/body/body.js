fc4aebf06ffd89e7bca8adf198ebaad0b498fbbb
Flynn Owen <flynnoo49@gmail.com>
2022-11-20 21:00:31 +1300

    resize bodies

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -5,7 +5,7 @@ function CustomBody(props) {
 
   return (
     <Fade in={true} timeout={{ enter: 1000 }}>
-      <Box width={{xs:"90%", md:"100%"}}>
+      <Box>
         <Paper elevation={3} sx={{ mt: 5, mb: 5, padding:2, textAlign: "center" }}>
           <Typography variant="h3" padding="30px">&#123;{props.title}&#125;</Typography>
           <Divider variant="middle" sx={{ mt: 3 }} />
